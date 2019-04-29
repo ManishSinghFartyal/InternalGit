9f24f629d4f2f4f2d4bc6c4ba679e4bf5f4ffd08
Manish Singh <manishfartyal9090@gmail.com>
2019-04-29 12:47:40 +0530

    Template change for running code online by candidate

--- code/51/PrimeNumber.java
+++ code/51/PrimeNumber.java
@@ -1,12 +1,22 @@
 import java.util.Scanner; 
 public class PrimeNumber 
 { 
-    public static void main(String args[]) 
-      { 
-
-      Scanner s=new Scanner(System.in);                   
-      num =s.nextInt(); 
-      //Write your code from here
-      } 
-} 
-                                
+                public static void main(String args[]) 
+             { 
+                  int num,b,c; 
+                  Scanner s=new Scanner(System.in); 
+                  num =s.nextInt(); 
+                  b=1; 
+                  c=0; 
+                   while(b<= num) 
+                      { 
+                          if((num%b)==0) 
+                             c=c+1; 
+                             b++; 
+                      } 
+                       if(c==2) 
+                       System.out.println("Number is prime"); 
+                       else 
+                       System.out.println("Number is not prime"); 
+             } 
+}
